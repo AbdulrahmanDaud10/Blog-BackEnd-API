8daad68a46310ab1156a248f31521d3933136e65
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-04-11 16:25:42 +0300

    Project migration Addition and project wrap up

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/AbdulrahmanDaud10/fullstack-project/api/controllers"
+	"github.com/AbdulrahmanDaud10/fullstack-project/api/seed"
+	"github.com/joho/godotenv"
+)
+
+var server = controllers.Server{}
+
+func Run() {
+
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+
+	server.IntializeDB(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(server.DB)
+
+	server.Run(":8080")
+
+}
